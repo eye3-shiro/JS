88138cd28efc99dd62eb519d79abc212133e8334
eye3-shiro <a.1627.work@gmail.com>
2025-08-08 13:22:13 +0900

    3-9スライド9
    イベント発火

--- task.js
+++ task.js
@@ -3,3 +3,7 @@ console.log(lists);
 
 const pullDownButton = document.getElementById("lists")
 console.log(pullDownButton);
+
+window.addEventListener('load', function() {
+    const pullDownButton = document.getElementById("lists")
+})

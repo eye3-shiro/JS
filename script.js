ef6b937ac3c3601612b701e7389f53f645d409e1
eye3-shiro <a.1627.work@gmail.com>
2025-08-08 11:25:23 +0900

    docment.puerySelector
    スライド１８

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 window.alert("ブラウザオブジェクトの取得に成功！")
+
+document.getElementById("lists");
+console.log(document.getElementById("lists"));
+
+function getElement() {
+    let element = document.querySelector('#shopB');
+    console.log('選ばれたのは' + element.textContent + 'でした');
+    }

a64127cd55075fac217efb1812579e1516636ace
eye3-shiro <a.1627.work@gmail.com>
2025-08-08 12:30:32 +0900

    docment.querySelectorALL
    スライド２１

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ window.alert("ブラウザオブジェクトの取得に成功！")
 document.getElementById("lists");
 console.log(document.getElementById("lists"));
 
-function getElement() {
-    let element = document.querySelector('#shopB');
-    console.log('選ばれたのは' + element.textContent + 'でした');
+//function getElement() {
+//    let element = document.querySelector('#shopB');
+//    console.log('選ばれたのは' + element.textContent + 'でした');
+//    }
+
+    function getElement2() {
+        elements = document.querySelectorAll('.shop li');
+//lenはlengthの略
+        let len = elements.length;
+        for (let i = 0; i < len; i++) {
+            console.log(elements.item(i).textContent);
+        }
     }

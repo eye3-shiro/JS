ddb60bf466d5de8a549dad88c50fd4dc1296957c
eye3-shiro <a.1627.work@gmail.com>
2025-08-12 22:08:09 +0900

    JS11条件分岐
    HTML、CSSの更新
    JS条件分岐加筆

--- task.js
+++ task.js
@@ -7,25 +7,29 @@ const pullDownButton = document.getElementById("lists");
 console.log(pullDownButton); // 動作確認用
 
 // プルダウンメニュー（ul要素）を取得
-const pullDownMenu = document.getElementById("pull-down");
-console.log(pullDownMenu); // 動作確認用
+const pullDownParents = document.getElementById("pull-down");
+console.log(pullDownParents); // 動作確認用
 
 // マウスが乗ったら表示
 pullDownButton.addEventListener("mouseover", function(){
-    pullDownButton.setAttribute("style","background-color:blue;");
+    this.setAttribute("style","background-color:blue;");
     console.log("マウスオーバーで背景色を青に変更しました");
 })
 
 
 // プルダウンからマウスが離れたら非表示
 pullDownButton.addEventListener("mouseout", function(){
-    pullDownButton.removeAttribute("style","background-color:red;");
+    this.removeAttribute("style","background-color:red;");
     console.log("マウスアウトで背景色を元に戻しました");
 })
 
 // クリックされたら表示切り替え
-pullDownButton.addEventListener("click", function(){
-    pullDownButton.setAttribute("style","background-color:green;");
-    console.log("クリックで背景色を緑に変更しました");
-    pullDownMenu.classList.toggle("hidden");
+pullDownButton.addEventListener('click', function(){
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示")
+    }else{
+        pullDownParents.setAttribute("style","display:block;")
+        console.log("表示")
+    }
 })
